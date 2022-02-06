6e8447a9341f741ff8bcd31640fbc914e8eec851
Alireza Naseri <alireza.real98@gmail.com>
2022-02-06 22:10:55 +0330

    Add menu to batch script

--- Locker.bat
+++ Locker.bat
@@ -6,10 +6,36 @@ cd /D %~DP0
 echo.
 echo Hi :)
 echo.
+goto menu
 
 set local=D:\myfolder
 if exist %local% goto lock
 if not exist %local% goto MDmyfolder
+
+:menu
+echo "                  Menu                    "
+echo.
+echo 1. Create Folder
+echo 2. Lock Folder
+echo 3. Unlock Folder
+echo 4. Remove Folder
+echo 5. Exit
+echo.
+echo.
+Set /p Select=Please select a number from the menu: 
+echo %Select%
+if %Select% == 1 goto MDmyfolder
+else if %Select% == 2 goto Lock
+else if %Select% == 3 goto Unlock
+else if %Select% == 4 goto Remove
+else if %Select% == 5 goto End
+else (
+    cls
+    echo Invalid choice, try again!
+    timeout /t 7
+    cls
+    goto menu
+)
 
 :MDmyfolder
 md D:\myfolder
@@ -28,7 +54,9 @@ attrib +h +s lock
 echo folder locked!
 goto end
 
-:end
+:End
 echo.
 echo See you soon!
+timeout /t 5 
+Exit Locker.bat
 echo.
